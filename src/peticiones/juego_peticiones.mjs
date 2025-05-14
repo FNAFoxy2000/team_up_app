db438f877b63161d1ee817890d45b141a83a1efc
Sickospren <lopezllorentem@gmail.com>
2025-05-15 01:45:27 +0200

    juego no encontrado

--- src/peticiones/juego_peticiones.mjs
+++ src/peticiones/juego_peticiones.mjs
@@ -6,8 +6,8 @@ export async function getDatosJuego(nombreJuego){
         return respuesta.data
     }catch(err){
         console.error('Error al obtener los datos del juego:', err);
-        // window.location.href = '/error' // puedes o redireccionar a /error (salta 404)
-        //throw err; // si lanzas el error te manda a la pagina de juego no encontrado
+        // window.location.href = '/error' // puedes o redireccionar a /error (salta 404) o hacer throw err;
+        throw err; // si lanzas el error te manda a la pagina de juego no encontrado
         //return []; //con esto da error
     }
 }
