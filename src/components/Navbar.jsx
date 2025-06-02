039e7f7fa8789b6b6f8f0fe88ab4edce90e4c836
Sickospren <lopezllorentem@gmail.com>
2025-06-03 01:16:24 +0200

    ajustes navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,7 +61,6 @@ function Navbar() {
         <li><Link to="/busqueda">Búsqueda</Link></li>
         <li><a href="/chat">Chats</a></li>
         <li><a href="/guias/crear">Guias</a></li>
-        <li><a href="/grid">Niveles</a></li>
       </ul>
 
 
